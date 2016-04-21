2af846054cbce0d2bf3517a27e0639ed5d712a31
Jeff Olson <olson.jeffery@gmail.com>
2016-04-21 05:41:07 -0500

    add spacemacs file, remove .emacs.d and .emacs

--- emacs.d/elpa/async-20150412.639/async-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "async" "20150412.639" "Asynchronous processing in Emacs" 'nil)
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- emacs.d/elpa/nav-20120507.7/nav-autoloads.el
+++
@@ -1,32 +0,0 @@
-;;; nav-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "ack" "ack.el" (21780 54859 0 0))
-;;; Generated autoloads from ack.el
-
-(defvar ack-history nil)
-
-;;;***
-
-;;;### (autoloads nil "nav" "nav.el" (21780 54859 0 0))
-;;; Generated autoloads from nav.el
-
-(autoload 'nav "nav" "\
-Opens Nav in a new window to the left of the current one.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("nav-pkg.el") (21780 54859 818331 0))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; nav-autoloads.el ends here
